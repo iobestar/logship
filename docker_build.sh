94fdc18a7924961a34f6764af45b34775ddf0785
Ivica Obestar <ivica.obestar@infobip.com>
2019-10-04 11:16:56 +0200

    fixed GOOS env to linux

--- docker_build.sh
+++ docker_build.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 GIT_COMMIT=$(git rev-list -1 HEAD)
 rm -f logship
-GOOS=$os GOARCH=amd64 go build -ldflags "-X main.Version=$VERSION -X main.Revision=$GIT_COMMIT -s -w"  ./cmd/logship
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Version=$VERSION -X main.Revision=$GIT_COMMIT -s -w"  ./cmd/logship
 upx --brute ./logship
 docker rmi logship:$VERSION
 docker build . -t logship:$VERSION
